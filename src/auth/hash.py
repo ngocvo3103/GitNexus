c75c731976cb85a13bb2eb9873c7ca4456d62e63
test <test@test>
2026-03-30 11:26:28 +0700

    initial commit

--- src/auth/hash.py
+++
@@ -1,9 +0,0 @@
-
-def hash_password(password):
-    return "hashed_" + password
-
-def compare_hash(plain, hashed):
-    return hash_password(plain) == hashed
-
-def generate_salt():
-    return "salt_" + str(id(object()))
