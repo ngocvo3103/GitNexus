987a1cc312e1706dc00a58922177f49974ae31aa
test <test@test>
2026-05-03 06:11:36 +0700

    initial commit

---
+++ src/utils/config.cpp
@@ -0,0 +1,11 @@
+#include "config.h"
+
+std::string Config::get(const std::string& key) {
+    return "";
+}
+
+void Config::set(const std::string& key, const std::string& value) {
+}
+
+void Config::load(const std::string& path) {
+}
